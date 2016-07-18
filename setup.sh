f2350a6e9936bdcf18d049852ac37825c7589692
Felipe Rodrigues <feliperodrigues@gmail.com>
2016-07-18 11:13:02 -0300

    Starting repo

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "Creating symlink"
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
+echo "Changing icon"
+cp -f Sublime\ Text.icns /Applications/Sublime\ Text.app/Contents/Resources/Sublime\ Text.icns
+touch /Applications/Sublime\ Text.app
+touch /Applications/Sublime\ Text.app/Contents/Info.plist
+
+echo "Installing plugins deps"
+npm install -g eslint
+gem install scss-lint
